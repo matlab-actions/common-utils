f269bd830d861ea0cc6adc259801f6bc0ee1f7c3
Kapil Gupta <kapilg@mathworks.com>
2026-05-27 00:46:19 +0530

    update variable name

--- plugins/+matlab/+unittest/+internal/+services/+plugins/TestResultsSummaryPluginService.m
+++ plugins/+matlab/+unittest/+internal/+services/+plugins/TestResultsSummaryPluginService.m
@@ -4,7 +4,7 @@ classdef TestResultsSummaryPluginService < matlab.buildtool.internal.services.ci
     methods
         function plugins = providePlugins(~, ~)
             % Check if MATLAB Test license is available
-            if strcmpi(getenv("MW_GENERATE_SUMMARY"), "true") && license('test', 'matlab_test')
+            if strcmpi(getenv("MW_INPUT_GENERATE_SUMMARY"), "true") && license('test', 'matlab_test')
                 plugins = testframework.TestResultsSummaryPlugin();
             else
                 plugins = matlab.unittest.plugins.TestRunnerPlugin.empty(1,0);
